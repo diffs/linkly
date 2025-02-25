95b2d4df923eddfc336aa9fe37279da8b55985c3
Rama Patel <me@rexsdev.com>
2025-02-25 16:41:55 -0500

    Update server.go to search message entirely

--- server/server.go
+++ server/server.go
@@ -1,63 +1,72 @@
 package server
 
 import (
-	"github.com/bwmarrin/discordgo"
-	"github.com/sirupsen/logrus"
-	"linkly/handlers"
-	"regexp"
+    "linkly/handlers"
+    "regexp"
+
+    "github.com/bwmarrin/discordgo"
+    "github.com/sirupsen/logrus"
 )
 
-var linkDetection = regexp.MustCompile(`https?://[^\s]+`)
+var linkDetection = regexp.MustCompile(`https?://\S+`)
 
 type Server interface {
-	Start() error
+    Start() error
 }
 
 type server struct {
-	session *discordgo.Session
-	log     *logrus.Logger
-	handler handlers.Handler
+    session *discordgo.Session
+    log     *logrus.Logger
+    handler handlers.Handler
 }
 
 func NewServer(session *discordgo.Session, handler handlers.Handler) Server {
-	return &server{
-		session: session,
-		log:     logrus.New(),
-		handler: handler,
-	}
+    return &server{
+        session: session,
+        log:     logrus.New(),
+        handler: handler,
+    }
 }
 
 func (s *server) Start() error {
-	s.session.AddHandler(s.messageCreate)
-	s.session.ShouldReconnectOnError = true
-	s.session.Identify.Intents = discordgo.IntentsGuildMessages
+    s.session.AddHandler(s.messageCreate)
+    s.session.ShouldReconnectOnError = true
+    s.session.Identify.Intents = discordgo.IntentsGuildMessages
+    s.session.Identify.Intents |= discordgo.IntentsMessageContent
 
-	// Open a websocket connection to Discord and begin listening.
-	err := s.session.Open()
-	if err != nil {
-		s.log.WithError(err).Error("failed to open ws connection to Discord")
-		return err
-	}
+    // Open a websocket connection to Discord and begin listening.
+    err := s.session.Open()
+    if err != nil {
+        s.log.WithError(err).Error("failed to open ws connection to Discord")
+        return err
+    }
 
-	if s.session.State != nil {
-		s.log.WithField("user", s.session.State.User.ID).Info("Connected to discord!")
-	}
-	return nil
+    if s.session.State != nil {
+        s.log.WithField("user", s.session.State.User.ID).Info("Connected to discord!")
+    }
+    return nil
 }
 
 func (s *server) messageCreate(sess *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	if m.Author.ID == sess.State.User.ID {
-		return
-	}
+    // Ignore all messages created by the bot itself
+    if m.Author.ID == sess.State.User.ID {
+        return
+    }
 
-	// Check if the message content matches the URL pattern
-	if linkDetection.MatchString(m.Content) {
-		if response := s.handler.HandleLink(m.Content); response != nil {
-			if _, err := sess.ChannelMessageSendComplex(m.ChannelID, response); err != nil {
-				s.log.WithError(err).Error("failed to send message")
-				return
-			}
-		}
-	}
+    link := linkDetection.FindString(m.Content)
+    if link == "" {
+        return
+    }
+
+    // Check if the message content matches the URL pattern
+    response := s.handler.HandleLink(link)
+    if response == nil {
+        return
+    }
+
+    _, err := sess.ChannelMessageSendComplex(m.ChannelID, response)
+    if err != nil {
+        s.log.WithError(err).Error("failed to send message")
+        return
+    }
 }
